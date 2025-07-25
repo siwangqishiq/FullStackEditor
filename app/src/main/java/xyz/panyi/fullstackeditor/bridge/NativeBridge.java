7b9fd335f7df699d7a256654f08b74a4980adf3e
panyi <panyi@yixin.im>
2025-07-25 11:37:35 +0800

    更新ffmpeg导入

--- app/src/main/java/xyz/panyi/fullstackeditor/bridge/NativeBridge.java
+++ app/src/main/java/xyz/panyi/fullstackeditor/bridge/NativeBridge.java
@@ -3,12 +3,13 @@ package xyz.panyi.fullstackeditor.bridge;
 public class NativeBridge {
     static {
         System.loadLibrary("avcodec");
+        System.loadLibrary("avfilter");
+        System.loadLibrary("avformat");
         System.loadLibrary("avutil");
+        System.loadLibrary("swresample");
+        System.loadLibrary("swscale");
         System.loadLibrary("fullstackeditor");
     }
-
-
-    public static native String stringFromJNI();
 
     public static native String ffmpegVersion();
 }
